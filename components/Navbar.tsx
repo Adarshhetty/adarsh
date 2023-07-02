62eb86362770a1d8e8f48c238cb6423fd515514c
Adarshhetty <95522378+Adarshhetty@users.noreply.github.com>
2023-07-02 11:57:36 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
         <Image  className='w-[10rem] h-[10rem]' src={logo} alt='Logo'/>
         </div>
         <div className='mt-14 mr-12'>
-          <a href="Adarsh-resume.pdf" download>
+          <a href="Adarsh-Resume.pdf" download>
         <button className=" hover:scale-110 transition-all duration-200 ease-in-out bg-white shadow hover:shadow-md text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
   <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
   <span>Resume</span>
