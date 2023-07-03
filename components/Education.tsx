17f46c41ab84e22c5914ffd1c1d9298b63265f59
Adarshhetty <95522378+Adarshhetty@users.noreply.github.com>
2023-07-03 07:58:49 +0530

    Update Education.tsx

--- components/Education.tsx
+++ components/Education.tsx
@@ -117,11 +117,7 @@ const Education = () => {
                 disabled>
                 CSS
               </button>
-              <button
-                className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-                disabled>
-                REACT
-              </button>
+
               <button
                 className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
                 disabled>
@@ -138,6 +134,11 @@ const Education = () => {
               FRAMEWORKS
             </h3>
             <p className=" flex-wrap font-normal text-gray-400">
+            <button
+                className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                disabled>
+                REACT
+              </button>
               <button
                 type="button"
                 className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
