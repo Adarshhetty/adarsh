d07937e0626c285ea8b7d8e6d99d0feae99f42af
Adarsh J Shetty <95522378+Adarshhetty@users.noreply.github.com>
2023-09-13 20:38:47 +0530

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,9 +5,10 @@ import disney from '../assets/disney.png'
 import gym from '../assets/gym.png'
 import Image from 'next/image'
 import Divider from './Divider'
+import dotSell from '../assets/dotsell.png'
 const Projects = () => {
   return (
-    <section id='projects' className=" min-h-screen py-3 mb-14 text-white">
+    <section id='projects' className=" min-h-screen ml-4 mr-4 py-3 mb-14 text-white">
          <Divider/>
          <div className="text-center mt-4">
         <h3 className="text-4xl font-bold hover:underline ">Projects</h3>
@@ -15,6 +16,25 @@ const Projects = () => {
       <div className=' relative  lg:left-0 md:left-14 '>
         
 <div className='relative gap-14 lg:mb-8 lg:flex md:flex sm:items-center    grid-cols-2 justify-center mt-14'>
+<div className="max-w-md mb-5 border border-gray-200 rounded-lg shadow bg-gray-800 dark:border-gray-700">
+    
+    <Image className="rounded-t-lg" src={dotSell} alt="" />
+
+<div className="p-5">
+    
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-white"> DotSell :MERN ecommerce website</h5>
+  
+    <p className="mb-3  font-normal text-gray-400">This ecommerce comprises of variety of clothings and its related accessories in single website.</p>
+    <a href="https://dot-sell-ecommerce-store.onrender.com/" className="inline-flex mr-8 items-center px-3 py-2 text-sm font-medium border border-gray-200 text-center text-white bg-gray-500 rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        Visit
+        <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+    </a>
+    <a href="https://github.com/Adarshhetty/DotSell" className="inline-flex items-center px-3 py-2 text-sm font-medium border border-gray-200 text-center text-white bg-gray-800 rounded-lg hover:bg-gray-500 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+       Github
+        <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+    </a>
+</div>
+</div>
 <div className="max-w-md mb-5 border border-gray-200 rounded-lg shadow bg-gray-800 dark:border-gray-700">
     
         <Image className="rounded-t-lg" src={car} alt="" />
